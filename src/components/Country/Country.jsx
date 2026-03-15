039b2b2eff7680a807f1decb3f5a6950b4573bc3
Ruhit Paul <ruhitpaul18@gmail.com>
2026-03-15 12:36:10 +0600

    total country visited function added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitiedCountries }) => {
     const [isVisited, setIsVisited] = useState(false)
 
     const handleVisited = () => {
-        if (isVisited) {
-            setIsVisited(false)
-        } else {
-            setIsVisited(true)
-        }
+        setIsVisited(!isVisited);
+        handleVisitiedCountries(country);
     }
     
     return (
-        <div className='country'>
+        <div className={`country ${isVisited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,17 +1,29 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css';
 
 const Countries = ({ countriesPromise }) => {
+    const [visitedCountries, setVisitedCountries] = useState([])
+
+    const handleVisitiedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+    }
+
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries
 
     return (
         <div>
             <h1>In The Countries: {countries.length}</h1>
+            <h3>Total Country Visited: {visitedCountries.length}</h3>
             <div className='countries'>
                 {
-                    countries.map((country) => <Country key={country.cca3.cca3} country={country}></Country>)
+                    countries.map((country) => <Country 
+                    key={country.cca3.cca3} 
+                    country={country}
+                    handleVisitiedCountries={handleVisitiedCountries}
+                    ></Country>)
                 }
             </div>
         </div>
