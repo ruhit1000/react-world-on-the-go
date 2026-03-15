6adfe5126ada86c9c8de197bc32a91a4f057f163
Ruhit Paul <ruhitpaul18@gmail.com>
2026-03-15 09:27:44 +0600

    Added styles to countries, conditionally rendered area status and visit button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({ country }) => {
+    const [isVisited, setIsVisited] = useState(false)
+
+    const handleVisited = () => {
+        if (isVisited) {
+            setIsVisited(false)
+        } else {
+            setIsVisited(true)
+        }
+    }
     
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Capital: {country.capital.capital}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000? '(Big Country)': '(Small Country)'}</p>
+            <button onClick={handleVisited}>{isVisited? "Visited" : "Not Visited"}</button>
         </div>
     );
 };
